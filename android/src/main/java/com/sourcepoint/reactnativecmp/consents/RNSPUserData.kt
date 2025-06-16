4d4e9c5b4d58336107673b995897a339bc7c65a3
André Herculano <andresilveirah@gmail.com>
2025-06-16 09:27:30 +0200

    feat: implement Preferences on Android native

--- android/src/main/java/com/sourcepoint/reactnativecmp/consents/RNSPUserData.kt
+++ android/src/main/java/com/sourcepoint/reactnativecmp/consents/RNSPUserData.kt
@@ -1,7 +1,9 @@
 package com.sourcepoint.reactnativecmp.consents
 
+import com.facebook.react.bridge.Arguments.createArray
 import com.facebook.react.bridge.Arguments.createMap
 import com.facebook.react.bridge.ReadableMap
+import com.sourcepoint.cmplibrary.model.exposed.PreferencesConsent
 import com.sourcepoint.cmplibrary.model.exposed.SPConsents
 
 interface RNMappable {
@@ -10,15 +12,18 @@ interface RNMappable {
 
 data class RNSPUserData(
   val gdpr: RNSPGDPRConsent?,
-  val usnat: RNSPUSNatConsent?
+  val usnat: RNSPUSNatConsent?,
+  val preferences: RNSPPreferencesConsent?
 ): RNMappable {
   constructor(spData: SPConsents): this(
     gdpr = spData.gdpr?.let { RNSPGDPRConsent(gdpr = it.consent)},
-    usnat = spData.usNat?.let { RNSPUSNatConsent(usnat = it.consent)}
+    usnat = spData.usNat?.let { RNSPUSNatConsent(usnat = it.consent)},
+    preferences = spData.preferences?.let { RNSPPreferencesConsent(preferences = it.consent) }
   )
 
   override fun toRN(): ReadableMap = createMap().apply {
     gdpr?.let { putMap("gdpr", it.toRN()) }
     usnat?.let { putMap("usnat", it.toRN()) }
+    preferences?.let { putMap("preferences", it.toRN()) }
   }
 }
