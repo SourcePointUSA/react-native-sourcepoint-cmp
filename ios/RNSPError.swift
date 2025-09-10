7c49dedc2ffa02c252bee6fae1a733eb8ba23dfc
André Herculano <andresilveirah@gmail.com>
2025-09-10 15:29:11 +0200

    iOS: expand RNSPErrorName's

--- ios/RNSPError.swift
+++ ios/RNSPError.swift
@@ -8,16 +8,55 @@
 import ConsentViewController
 
 public enum RNSPErrorName: String {
-  case unknown = "Unknown"
+    case unknown = "Unknown"
+    case noInternetConnection = "NoInternetConnection"
+    case loadMessagesError = "LoadMessagesError"
+    case renderingAppError = "RenderingAppError"
+    case reportActionError = "ReportActionError"
+    case reportCustomConsentError = "ReportCustomConsentError"
+    case androidNoIntentFound = "AndroidNoIntentFound" // (iOS: no equivalent)
 
-  init(_ error: SPError) {
-    switch error {
-    default:
-      self = .unknown
+    init(_ error: SPError) {
+        switch error {
+
+        // MARK: - No Internet
+        case is NoInternetConnection:
+            self = .noInternetConnection
+
+        // MARK: - Load Messages errors (/get_messages, /message/gdpr, /message/ccpa)
+        case is InvalidResponseGetMessagesEndpointError,
+             is InvalidResponseMessageGDPREndpointError,
+             is InvalidResponseMessageCCPAEndpointError:
+            self = .loadMessagesError
+
+        // MARK: - Rendering App / WebView / JSReceiver issues
+        case is RenderingAppError,
+             is RenderingAppTimeoutError,
+             is UnableToInjectMessageIntoRenderingApp,
+             is UnableToLoadJSReceiver,
+             is WebViewError,
+             is WebViewConnectionTimeOutError,
+             is InvalidEventPayloadError,
+             is InvalidOnActionEventPayloadError,
+             is InvalidURLError:
+            self = .renderingAppError
+
+        // MARK: - Report Action
+        case is ReportActionError,
+             is InvalidReportActionEvent:
+            self = .reportActionError
+
+        // MARK: - Custom Consent reporting
+        case is InvalidResponseCustomError,
+             is InvalidResponseDeleteCustomError,
+             is PostingCustomConsentWithoutConsentUUID:
+            self = .reportCustomConsentError
+
+        default:
+            self = .unknown
+        }
     }
-  }
 }
-
 
 @objcMembers public class RNSPError: NSObject {
   public let name: RNSPErrorName
